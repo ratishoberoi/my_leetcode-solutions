159f959362f1b85c01ca45281bc06934ecdf11f9
Ratish Oberoi <ratishoberoi3993@gmail.com>
2026-05-19 15:49:58 +0530

    commit: Added LeetCode solutions

---
+++ 33.py
@@ -0,0 +1,29 @@
+#BRUTE FORCE 
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
+    
+#BINARY SEARCH
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:      
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:                            
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1         
+        return -1   
